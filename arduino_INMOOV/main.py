1e971a890d8969c4c314499c7a92a93507178f38
1barrier <160951045+1barrier@users.noreply.github.com>
2024-12-06 15:31:28 -0800

    Caleb - 12/6/2024

--- arduino_INMOOV/main.py
+++ arduino_INMOOV/main.py
@@ -1,16 +1,9 @@
-import threading
 from board import Board
 
-activeThreads = []
-
 if __name__ =="__main__":
-    board = Board()
-
-    boardThread = threading.Thread(target=board.run(),args=())
-    activeThreads.append(boardThread)
-
-    for i in activeThreads:
-        i.start()
-    
-    for i in activeThreads:
-        i.join()
+    try:
+        board = Board()
+        print("RUNNING MAIN")
+        board.run()
+    except Exception:
+        print("ERROR: "+Exception)
